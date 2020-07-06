c3655533423a85d9789ad7e31e177cb6f6c8648e
mlppookn <604673386@qq.com>
2020-07-06 09:08:22 +0800

    vue

---
+++ navigation.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+      <el-menu-item index="1"><router-link to="/goods">图书列表</router-link></el-menu-item>
+      <el-menu-item index="2"><router-link to="/borrow">推荐图书</router-link></el-menu-item>
+      <el-menu-item index="3"><router-link to="/rent">我的上架</router-link></el-menu-item>
+      <el-menu-item index="4"><router-link to="/publishArticle">我要上架</router-link></el-menu-item>
+      <el-menu-item index="5"><router-link to="/person">个人中心</router-link></el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'navigation'
+}
+</script>
+
+<style scoped>
+
+</style>
